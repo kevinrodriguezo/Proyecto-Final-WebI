943e37032299b895e77783a221aace203d04c0ac
kevinrodriguezo <kevin.1296@hotmail.com>
2017-04-29 15:02:10 -0600

    Avcances login

--- application/controllers/usuario.php
+++ application/controllers/usuario.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User extends CI_Controller {
+class Usuario extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
